fd8c71df79e5c115acb2a4be1821729a53f55f11
RamSSi <001229aram@gmail.com>
2022-10-02 01:57:07 +0900

    스프링부트 게시판 무작정 따라하기 - 글 삭제 처리 end

--- src/main/java/com/springbootproject/board/controller/BoardController.java
+++ src/main/java/com/springbootproject/board/controller/BoardController.java
@@ -4,6 +4,7 @@ import com.springbootproject.board.Service.BoardService;
 import com.springbootproject.board.entity.Board;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,8 +26,29 @@ public class BoardController {
 
         boardService.write(board);
         System.out.println(board.getTitle());
-
-        return "";
+//        return "redirect:/board/list";
+        return "redirect:/board/view?id=" + board.getId();
     }
 
+    @GetMapping("/board/list")
+    public String boardList(Model model) {
+
+        model.addAttribute("list", boardService.boardList());
+
+        return "boardlist";
+    }
+
+    @GetMapping("/board/view") // localhost:8080/board/view?id=1
+    public String boardView(Model model, Integer id) {
+
+        model.addAttribute("article", boardService.boardView(id));
+
+        return "boardview";
+    }
+
+    @GetMapping("board/delete")
+    public String boardDelete(Integer id) {
+        boardService.boardDelete(id);
+        return "redirect:/board/list";
+    }
 }
